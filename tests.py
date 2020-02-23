ecbf396f5a005ad133f6659a203eebf5330dc737
CS362W2019 <cs362w2019@gmail.com>
2020-02-23 06:15:32 -0800

    Fix indentation

--- tests.py
+++ tests.py
@@ -5,12 +5,12 @@ import task
 class TestCase(unittest.TestCase):
 
 	def test1(self):
-        expected = "success"
-        self.assertEqual(expected, task.firstrun())
+		expected = "success"
+		self.assertEqual(expected, task.firstrun())
 
-    def test2(self):
-        expected = "failure"
-        self.assertNotEqual(expected, task.firstrun())
+	def test2(self):
+		expected = "failure"
+		self.assertNotEqual(expected, task.firstrun())
 
 
 if __name__ == '__main__':
