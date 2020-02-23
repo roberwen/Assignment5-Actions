bc96ea05ba7f6a46ba0043c0852f8ce5c182e8de
CS362W2019 <cs362w2019@gmail.com>
2020-02-23 06:20:43 -0800

    Picky

--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
 import unittest
 import classroom_manager
 
-
 class TestCase(unittest.TestCase):
     def test1(self):
         expected = "success"
@@ -10,7 +9,6 @@ class TestCase(unittest.TestCase):
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, classroom_manager.firstrun())
-
 
 if __name__ == '__main__':
     unittest.main()
